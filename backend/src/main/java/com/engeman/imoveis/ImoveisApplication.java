3047877e5190827017d546b4cd9455ed89b50244
Andre <andre_messi014@hotmail.com>
2026-07-04 08:28:57 -0300

    debug

--- backend/src/main/java/com/engeman/imoveis/ImoveisApplication.java
+++ backend/src/main/java/com/engeman/imoveis/ImoveisApplication.java
@@ -7,7 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ImoveisApplication {
 
 	public static void main(String[] args) {
+		System.out.println("### DEBUG SPRING_DATASOURCE_URL = [" + System.getenv("SPRING_DATASOURCE_URL") + "]");
+		System.out.println("### DEBUG tamanho = " + (System.getenv("SPRING_DATASOURCE_URL") == null ? "null" : System.getenv("SPRING_DATASOURCE_URL").length()));
 		SpringApplication.run(ImoveisApplication.class, args);
 	}
-
 }
